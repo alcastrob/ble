4ffb04e8f8d06cfe340a65e90342dc365798f689
Angel Castro <al.castro@gmail.com>
2019-06-12 10:59:47 +0200

    Basic app working.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A proof-of-concept Ionic app for using the Bluetooth Low Energy (BLE) stack.
 
 # Installation steps
 
-- npm install -g ionic
+- npm install -g ionic cordova
 - npm install @ionic/angular @angular/cli
 - npm install
 - ionic cordova platform add android
--- ble/src/app/home/home.page.ts
+++ ble/src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { BLE } from '@ionic-native/ble/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private ble: BLE) {}
+
+  constructor() {}
+
 }
